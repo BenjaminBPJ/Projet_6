46f894748e03ddfe2e1662af1c6e7afb7ee9cd36
BenjaminBPJ <pierrejeanbenjamin@hotmail.fr>
2020-11-27 23:36:48 +0100

    initial commit

---
+++ backend/controllers/sauces.js
@@ -0,0 +1,106 @@
+const Thing = require('../models/sauces');
+
+exports.createThing = (req, res, next) => {
+    const thing = new Thing({
+        userId: req.body.userId,
+        name: req.body.name,
+        manufacturer: req.body.manufacturer,
+        description: req.body.description,
+        mainPepper: req.body.mainPepper,
+        imageUrl: req.body.imageUrl,
+        heat: req.body.heat,
+        likes: req.body.likes,
+        dislikes: req.body.dislikes,
+        userLiked: req.body.userLiked,
+        userDisliked: req.body.userDisliked,
+    });
+    thing.save().then(
+        () => {
+            res.status(201).json({
+                message: 'Post saved successfully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+};
+
+exports.getOneThing = (req, res, next) => {
+    Thing.findOne({
+        id: req.params.id
+    }).then(
+        (thing) => {
+            res.status(200).json(thing);
+        }
+    ).catch(
+        (error) => {
+            res.status(404).json({
+                error: error
+            });
+        }
+    );
+};
+
+exports.modifyThing = (req, res, next) => {
+    const thing = new Thing({
+        id: req.params.id,
+        userId: req.body.userId,
+        name: req.body.name,
+        manufacturer: req.body.manufacturer,
+        description: req.body.description,
+        mainPepper: req.body.mainPepper,
+        imageUrl: req.body.imageUrl,
+        heat: req.body.heat,
+        likes: req.body.likes,
+        dislikes: req.body.dislikes,
+        userLiked: req.body.userLiked,
+        userDisliked: req.body.userDisliked,
+    });
+    Thing.updateOne({ id: req.params.id }, thing).then(
+        () => {
+            res.status(201).json({
+                message: 'Thing updated successfully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+};
+
+exports.deleteThing = (req, res, next) => {
+    Thing.deleteOne({ id: req.params.id }).then(
+        () => {
+            res.status(200).json({
+                message: 'Deleted!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+};
+
+exports.getAllStuff = (req, res, next) => {
+    Thing.find().then(
+        (things) => {
+            res.status(200).json(things);
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+};
