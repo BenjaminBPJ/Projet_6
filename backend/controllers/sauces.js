ef1291f759dc54625e759869bef1c2d380d01062
BenjaminBPJ <pierrejeanbenjamin@hotmail.fr>
2020-12-09 11:50:33 +0100

    ajout des messages en cas d'echec

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -9,14 +9,14 @@ exports.createSauce = (req, res, next) => {
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     sauce.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
-        .catch(error => res.status(400).json({ error }));
+        .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
+        .catch(error => res.status(400).json({ message : `Impossible de créer cette sauce` }));
 };
 
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then((sauce) => { res.status(200).json(sauce); })
-        .catch((error) => { res.status(404).json({ error: error }); });
+        .catch((error) => { res.status(404).json({ message : `Cette sauce n'existe pas` }); });
 };
 
 exports.modifySauce = (req, res, next) => {
@@ -26,8 +26,8 @@ exports.modifySauce = (req, res, next) => {
             imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
         } : { ...req.body };
     Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
-        .catch(error => res.status(400).json({ error }));
+        .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
+        .catch(error => res.status(400).json({ message : `Impossible de modifier cette sauce` }));
 };
 
 exports.deleteSauce = (req, res, next) => {
@@ -36,8 +36,8 @@ exports.deleteSauce = (req, res, next) => {
             const filename = sauce.imageUrl.split('/images/')[1];
             fs.unlink(`images/${filename}`, () => {
                 Sauce.deleteOne({ _id: req.params.id })
-                    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
-                    .catch(error => res.status(400).json({ error }));
+                    .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
+                    .catch(error => res.status(400).json({ message : `Impossible de supprimer cette sauce` }));
             });
         })
         .catch(error => res.status(500).json({ error }));
@@ -46,5 +46,5 @@ exports.deleteSauce = (req, res, next) => {
 exports.getAllSauces = (req, res, next) => {
     Sauce.find()
         .then((sauces) => { res.status(200).json(sauces); })
-        .catch((error) => { res.status(400).json({ error: error }); });
+        .catch((error) => { res.status(400).json({ message : `Les sauces sont indisponibles` }); });
 };
